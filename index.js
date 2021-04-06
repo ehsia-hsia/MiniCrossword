9bc827a4e54547b37a9d1fd61d106a19f9b3ed25
ehsia-hsia <erica.hsia1@gmail.com>
2021-04-06 15:21:16 -0400

    forEach Function Call
    call check function for each word using array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const poem = new CrossWordKey(
+const word1 = new CrossWordKey(
   "_poem",
   "across1L1Input",
   "across1L2Input",
@@ -9,13 +9,40 @@ const poem = new CrossWordKey(
   "across1L5Input"
 );
 
-const hippo = new CrossWordKey(
+const word2 = new CrossWordKey(
   "hippo",
   "across2L1Input",
   "across2L2Input",
   "across2L3Input",
   "across2L4Input",
   "across2L5Input"
+);
+
+const word3 = new CrossWordKey(
+  "april",
+  "across3L1Input",
+  "across3L2Input",
+  "across3L3Input",
+  "across3L4Input",
+  "across3L5Input"
+);
+
+const word4 = new CrossWordKey(
+  "space",
+  "across4L1Input",
+  "across4L2Input",
+  "across4L3Input",
+  "across4L4Input",
+  "across4L5Input"
+);
+
+const word5 = new CrossWordKey(
+  "hahs_",
+  "across5L1Input",
+  "across5L2Input",
+  "across5L3Input",
+  "across5L4Input",
+  "across5L5Input"
 );
 function CrossWordKey(word, id1, id2, id3, id4, id5) {
   this.word = word;
@@ -46,6 +73,6 @@ function CrossWordKey(word, id1, id2, id3, id4, id5) {
 } //end object
 
 function runPlay() {
-  poem.check();
-  hippo.check();
+  const wordArray = [word1, word2, word3, word4, word5];
+  wordArray.forEach((word) => word.check());
 }
